bd4eaa4d1893cd75d679ade2f308c68375526476
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-01-18 14:20:42 +0700

    implement adminLTE for appearance

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
- Route::middleware('role:admin')->get('/dashboard', function () {
-     return 'Dashboaird';
- })->name('dashboard');
+Route::middleware('role:admin')->get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
